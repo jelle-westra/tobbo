a6d6dce3e8e5c6d02323ac83613c5c5a74813a0e
jelle-westra <jelwestra@gmail.com>
2025-04-01 16:44:32 +0200

    moved the intialization of geometry and mask to the post init

--- TO/topology.py
+++ TO/topology.py
@@ -7,9 +7,10 @@ class Topology:
     continuous: bool
     domain: Polygon
     density: float # [elements/squared domain unit]
-    geometry: MultiPolygon = None
-    mask: np.ndarray = None
 
     def __post_init__(self) :
         assert (self.domain.bounds[:2] == (0,0)), 'lower-left corner should be (0,0)'
         (_, _, *self.domain_size) = (_, _, self.domain_size_x, self.domain_size_y) = self.domain.bounds
+        
+        geometry: MultiPolygon = None
+        mask: np.ndarray = None
