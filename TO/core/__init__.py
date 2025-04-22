280e2310cf6bc6ad27266eace6559fc90c9f949a
jelle-westra <jelwestra@gmail.com>
2025-04-22 10:29:57 +0200

    updated imports

--- TO/core/__init__.py
+++ TO/core/__init__.py
@@ -1,11 +1,11 @@
 from .parameterization import Parameterization
 from .problem import ProblemInstance
-from .constraint import VolumeConstraint, DisconnectionConstraint, ConstraintEnforcement
+from .constraint import VolumeConstraint, DisconnectionConstraint, ConstraintEnforcement, Constraint
 from .topology import Topology
 from .experiment import run_experiment
 from .model import Model
 
 __all__ = [
     'Parameterization', 'ProblemInstance', 'Topology', 'Model', 'run_experiment',
-    'VolumeConstraint', 'DisconnectionConstraint', 'ConstraintEnforcement'
+    'VolumeConstraint', 'DisconnectionConstraint', 'ConstraintEnforcement', 'Constraint'
 ]
