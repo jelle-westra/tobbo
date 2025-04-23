09027fc5bdaa44026f65d30ed4e89fbb562103c4
Jelle Westra <jelwestra@gmail.com>
2025-04-23 14:07:09 +0200

    added arguments to the script

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+import argparse
+from contextlib import redirect_stdout
 import multiprocessing
 from tqdm import tqdm
 import os
-from contextlib import redirect_stdout
 
 import numpy as np
 from shapely.geometry import box
-
 
 from TO.core import Topology, ProblemInstance, run_experiment
 from TO.parameterizations.mmc import Capsules, MMCCenterpointsConfig
 from TO.problems.cantilever import create_horizontal_cantilever_problem
 
-NO_THREADS: int = 15
-NO_EXPERIMENTS: int = 30
 MIN_PER_EVAL: float = 2/500 # approximate [min/evaluations]
 
 budget: int = lambda d : 100*d
@@ -24,20 +22,29 @@ def setup_problem() -> ProblemInstance:
     )
     return create_horizontal_cantilever_problem(topology, parameterization)
 
-def run(seed: int):
-    problem = setup_problem()
-    with open(os.devnull, 'w') as fnull, redirect_stdout(fnull):
-        run_experiment(problem, budget(problem.parameterization.dimension), seed, 'multi-test')
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--threads', type=int)
+    parser.add_argument('--experiments', type=int, default=30)
+    parser.add_argument('--components', type=int, default=2)
+    return parser.parse_args()
 
 if __name__ == '__main__':
+    args = parse_args()
+
+    def run(seed: int):
+        problem = setup_problem(args.components)
+        with open(os.devnull, 'w') as fnull, redirect_stdout(fnull):
+            run_experiment(problem, budget(problem.parameterization.dimension), seed, 'multi-test')
+
     problem = setup_problem()
     print(f'Problem dimension: {problem.parameterization.dimension}')
     print(f'Budget: {budget(problem.parameterization.dimension)}')
-    eta = MIN_PER_EVAL * NO_EXPERIMENTS*budget(problem.parameterization.dimension) / NO_THREADS
+    eta = MIN_PER_EVAL * args.experiments*budget(problem.parameterization.dimension) / args.threads
     print(f'ETA : {eta:.1f}min')
     del problem
-    seeds = list(range(1, NO_EXPERIMENTS+1))
-    with multiprocessing.Pool(processes=NO_THREADS) as pool:
+    seeds = list(range(1, args.experiments+1))
+    with multiprocessing.Pool(processes=args.threads) as pool:
         with tqdm(total=len(seeds)) as pbar:
             for _ in pool.imap_unordered(run, seeds):
                 pbar.update(1)
