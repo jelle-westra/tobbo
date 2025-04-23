38782b0a9e92a9e91f82ae5c3c06a804b373294c
Jelle Westra <jelwestra@gmail.com>
2025-04-23 14:18:33 +0200

    made components global static variables; parameterization is constant anyways

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from TO.core import Topology, ProblemInstance, run_experiment
 from TO.parameterizations.mmc import Capsules, MMCCenterpointsConfig
 from TO.problems.cantilever import create_horizontal_cantilever_problem
 
+COMPONENTS = 4
+
 MIN_PER_EVAL: float = 2/500 # approximate [min/evaluations]
 
 budget: int = lambda d : 100*d
@@ -18,24 +20,23 @@ budget: int = lambda d : 100*d
 def setup_problem() -> ProblemInstance:
     topology = Topology(True, box(0, 0, 100, 50), 1.0)
     parameterization = Capsules(
-        topology, False, True, 2, MMCCenterpointsConfig, None, 1_000
+        topology, False, True, COMPONENTS, MMCCenterpointsConfig, None, 1_000
     )
     return create_horizontal_cantilever_problem(topology, parameterization)
+
+def run(seed: int):
+    problem = setup_problem()
+    with open(os.devnull, 'w') as fnull, redirect_stdout(fnull):
+        run_experiment(problem, budget(problem.parameterization.dimension), seed, 'multi-test')
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--threads', type=int)
     parser.add_argument('--experiments', type=int, default=30)
-    parser.add_argument('--components', type=int, default=2)
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_args()
-
-    def run(seed: int):
-        problem = setup_problem(args.components)
-        with open(os.devnull, 'w') as fnull, redirect_stdout(fnull):
-            run_experiment(problem, budget(problem.parameterization.dimension), seed, 'multi-test')
 
     problem = setup_problem()
     print(f'Problem dimension: {problem.parameterization.dimension}')
