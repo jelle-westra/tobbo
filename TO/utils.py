b6a9a1c26af37831faddf2e517babd89e1c7466b
Jelle Westra <jelwestra@gmail.com>
2025-04-25 10:57:10 +0200

    checking package status now also supports shell

--- TO/utils.py
+++ TO/utils.py
@@ -5,27 +5,40 @@ import subprocess
 from IPython.display import display, Markdown
 from typing import List
 
-def check_package_status() :
+def check_package_status(notebook: bool=True) :
     path = os.path.dirname(os.path.abspath(__file__))
     commit = subprocess.run(['git', 'rev-parse', 'HEAD'], capture_output=True, text=True)
 
     file_changes = subprocess.run(['git', 'diff', '--name-only', path], capture_output=True, text=True).stdout
     untracked_files = subprocess.run(['git', 'ls-files', '--others', '--exclude-standard', path], capture_output=True, text=True).stdout
     
-    if file_changes or untracked_files :
-        display(Markdown(
-            '<div class="alert alert-block alert-danger" style="font-size: 14px; border-radius: 10px">'
-            f'<h4>[NO GO] TO @ {commit.stdout}</h4>'
-            '<pre>  ⚠️ Changes found in the ./TO package, first commit changes before starting experiments.</pre>'
-            '</div>'
-        ))
-    else: 
-        display(Markdown(
-            '<div class="alert alert-block alert-success" style="font-size: 14px; border-radius: 10px">'
-            f'<h4>[GO] TO @ {commit.stdout}</h4>'
-            '<pre>  ✅ No changes found in the ./TO packge.</pre>'
-            '</div>'
-        ))
+    if (notebook):
+        if (file_changes or untracked_files):
+            display(Markdown(
+                '<div class="alert alert-block alert-danger" style="font-size: 14px; border-radius: 10px">'
+                f'<h4>[NO GO] TO @ {commit.stdout}</h4>'
+                '<pre>  ⚠️ Changes found in the ./TO package, first commit changes before starting experiments.</pre>'
+                '</div>'
+            ))
+            return False
+        else: 
+            display(Markdown(
+                '<div class="alert alert-block alert-success" style="font-size: 14px; border-radius: 10px">'
+                f'<h4>[GO] TO @ {commit.stdout}</h4>'
+                '<pre>  ✅ No changes found in the ./TO packge.</pre>'
+                '</div>'
+            ))
+    else:
+        if (file_changes or untracked_files) :
+            print(f'[NO GO] TO @ {commit.stdout}')
+            print('Changes found in the ./TO package, first commit changes before starting experiments.')
+            return False
+        else:
+            print(f'[GO] TO @ {commit.stdout}')
+            print('No changes found in the ./TO packge.')
+    with open(f'{commit.stdout}.version', 'w') as _ : ...
+    return True          
+
 
 def read_evals(name: str, seed: int, run: int=None) -> List[str] :
     path = os.path.join(os.path.abspath(''), f'results/{name}/{seed}/evals.dat' if not(run) else f'results/{name}/{seed}-{run}/evals.dat')
