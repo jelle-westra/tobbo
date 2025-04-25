48868ded8c120bc040b7fdc9bfa1fe52710c9387
Jelle Westra <jelwestra@gmail.com>
2025-04-25 10:33:16 +0200

    added parallelized experiments

--- TO/core/__init__.py
+++ TO/core/__init__.py
@@ -2,10 +2,10 @@ from .parameterization import Parameterization
 from .problem import ProblemInstance
 from .constraint import VolumeConstraint, DisconnectionConstraint, ConstraintEnforcement, Constraint
 from .topology import Topology
-from .experiment import run_experiment
+from .experiment import run_experiment, run_experiment_parallel
 from .model import Model
 
 __all__ = [
-    'Parameterization', 'ProblemInstance', 'Topology', 'Model', 'run_experiment',
+    'Parameterization', 'ProblemInstance', 'Topology', 'Model', 'run_experiment', 'run_experiment_parallel',
     'VolumeConstraint', 'DisconnectionConstraint', 'ConstraintEnforcement', 'Constraint'
 ]
--- TO/core/experiment.py
+++ TO/core/experiment.py
@@ -1,10 +1,20 @@
-from .problem import ProblemInstance
-
 import numpy as np
 import ioh
 import cma
 
+from collections.abc import Callable
+from contextlib import redirect_stdout
+import multiprocessing
+from threading import Thread
+from tqdm import tqdm
+from typing import List
+from time import sleep
 import os
+
+from .problem import ProblemInstance
+from .parameterization import Parameterization
+
+BUDGET_PER_DIMENSION: int = 100
 
 def run_experiment(
     problem: ProblemInstance,
@@ -57,3 +67,44 @@ def run_experiment(
 
     problem.reset()
     logger.close()
+
+def _run_instance(args):
+    (seed, problem_constructor, budget, name) = args
+    problem: ProblemInstance = problem_constructor()
+    with open(os.devnull, 'w') as fnull, redirect_stdout(fnull):
+        run_experiment(problem, budget, seed, name)
+
+def _run_progress_checker(seeds: List[int], budget: int, name: str):
+    path = lambda seed : f'results/{name}/{seed}/evals.dat'
+    n = len(seeds)
+    relative_progress = [0.]*n
+    print('\n'*n)
+    while any(p < 1. for p in relative_progress):
+        print('\033[F' * n, end='')  # moving cursor up
+        for (i, seed) in enumerate(seeds):
+            if not(relative_progress[i] == 1) and os.path.exists(path(seed)) :
+                with open(path(seed), 'rb') as f:
+                    relative_progress[i] = sum(1 for _ in f)/budget
+
+            pbar_length: int = 50
+            fill_length = int(relative_progress[i]*pbar_length)
+            pbar = '='*fill_length + (pbar_length-fill_length)*' '
+            print(f'[{i+1:02d}/{n:02d}] [' + pbar + f'] {relative_progress[i]*100:.1f}%')
+        sleep(1)
+
+
+def run_experiment_parallel(problem_constructor: Callable[[],ProblemInstance], budget_func: Callable[[Parameterization], int], name: str, experiments: int, threads: int):
+    problem: ProblemInstance = problem_constructor()
+    budget = budget_func(problem.parameterization)
+    print(f'Problem dimension: {problem.parameterization.dimension}')
+    print(f'Budget: {budget}')
+
+    seeds = list(range(1, experiments+1))
+    args = [(seed, problem_constructor, budget, name) for seed in seeds]
+    del problem
+
+    progress_thread = Thread(target=_run_progress_checker, args=(seeds, budget, name), daemon=True)
+    progress_thread.start()
+    
+    with multiprocessing.Pool(processes=threads) as pool:
+        for _ in pool.imap_unordered(_run_instance, args) : ...
