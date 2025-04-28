391202c1132a2000a69a8e9315a5ad5f55df69f1
jelle-westra <jelwestra@gmail.com>
2025-04-28 15:43:46 +0200

    added ConstraintMix to imports

--- TO/core/__init__.py
+++ TO/core/__init__.py
@@ -1,11 +1,11 @@
 from .parameterization import Parameterization
 from .problem import ProblemInstance
-from .constraint import VolumeConstraint, DisconnectionConstraint, ConstraintEnforcement, Constraint
+from .constraint import VolumeConstraint, DisconnectionConstraint, ConstraintEnforcement, Constraint, ConstraintMix
 from .topology import Topology
 from .experiment import run_experiment, run_experiment_parallel
 from .model import Model
 
 __all__ = [
     'Parameterization', 'ProblemInstance', 'Topology', 'Model', 'run_experiment', 'run_experiment_parallel',
-    'VolumeConstraint', 'DisconnectionConstraint', 'ConstraintEnforcement', 'Constraint'
+    'VolumeConstraint', 'DisconnectionConstraint', 'ConstraintEnforcement', 'Constraint', 'ConstraintMix'
 ]
