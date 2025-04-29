3ddf3cf9a9e0ba5d6a8d81667442ab53b4093669
jelle-westra <jelwestra@gmail.com>
2025-04-29 09:41:40 +0200

    only domain size are required now, instead of the user constructing the domain polygon on their own -> results in always rectangluar domain now

--- TO/core/topology.py
+++ TO/core/topology.py
@@ -1,20 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
-from shapely.geometry import Polygon, MultiPolygon
+from shapely.geometry import Polygon, MultiPolygon, box
 from shapely.plotting import plot_polygon
 
 from dataclasses import dataclass
+from typing import Tuple
 
 @dataclass
 class Topology:
     continuous: bool
-    domain: Polygon
+    domain_size: Tuple[float, float]
     density: float # [elements/squared domain unit]
 
     def __post_init__(self) :
-        assert (self.domain.bounds[:2] == (0,0)), 'lower-left corner should be (0,0)'
-        (_, _, *self.domain_size) = (_, _, self.domain_size_x, self.domain_size_y) = self.domain.bounds
+        (self.domain_size_x, self.domain_size_y) = self.domain_size
+        self.domain = box(0, 0, self.domain_size_x, self.domain_size_y)
         
         geometry: MultiPolygon = None
         mask: np.ndarray = None
